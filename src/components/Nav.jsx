1a33b888e8fad45142b63931d67181a14d5847a7
manotenudulkar <manotendulkar10@gmail.com>
2024-02-12 22:05:43 +0530

    navbar hamburger created

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { navLinks } from "../constants";
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 
 const Nav = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const toggleNav = () => {
+    setOpenModal(!openModal);
+  };
   return (
     <header className="padding-x py-8 absolute w-full z-10">
       <nav className="flex justify-between items-center max-container">
@@ -23,12 +27,40 @@ const Nav = () => {
           ))}
         </ul>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            onClick={toggleNav}
+          />
         </div>
+
         <div className="hidden lg:block">
           <p className="font-bold tracking-tight">Sign In / Sign Up</p>
         </div>
       </nav>
+      <div
+        className={` ${
+          openModal ? "flex" : "hidden"
+        } items-center fixed justify-center h-screen w-screen left-0 top-0 z-40 text-white bg-white`}
+      >
+        {openModal && (
+          <ul className="relative z-50">
+            {navLinks.map((item) => (
+              <li key={item.label} className="mb-4">
+                <a
+                  href={item.href}
+                  className="font-montserrat text-slate-gray text-lg leading-normal"
+                  onClick={toggleNav}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </header>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,8 +17,8 @@ const Hero = () => {
         <p className=" text-coral-red font-montserrat text-xl">
           Our Summer Collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className=" xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px]  max-sm:leading-[82px] font-bold">
+          <span className=" xl:bg-white xl:whitespace-nowrap relative z-[1] pr-10">
             The New Arrival
           </span>
           <br />
